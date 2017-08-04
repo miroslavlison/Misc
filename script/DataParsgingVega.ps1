e92dd0b1abd56fa07987a8407f741688b4edd676
Miroslav Lison (Modis Professional Services) <a-milis@microsoft.com>
2017-08-04 11:47:12 -0700

    Adding powershell script

---
+++ script/DataParsgingVega.ps1
@@ -0,0 +1,12 @@
+﻿$csvfile = Import-Csv 'C:\Users\a-milis\Source\Visualization\data\ICMBreakdown.csv'
+
+#$values =@([pscustomobject]@{Category="";Position="";Value=""})
+
+foreach ($i in $csvfile) {
+    
+    $values += @([pscustomobject]@{Category=$i.CreateDate;Position=0;Value=$i.Severity1})
+    $values += @([pscustomobject]@{Category=$i.CreateDate;Position=1;Value=$i.Severity2})
+    $values += @([pscustomobject]@{Category=$i.CreateDate;Position=2;Value=$i.Severity3})
+    
+}
+$values | Export-Csv -notypeinformation -Path C:\users\a-milis\Desktop\Data.csv
